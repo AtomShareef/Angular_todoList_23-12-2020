36f16abf9c97fcfdaf3876efd8fef82394f84ab7
AtomShareef <shareef.m@pacewisdom.com>
2020-12-24 08:28:57 +0530

    Angular advanced Todo list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AboutComponent } from './components/pages/about/about.component';
+import { TodosComponent } from './components/todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', component: TodosComponent}, //Adding router at last
+  { path : 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
